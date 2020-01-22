9e0ae03e04d2b8da308ed55e77f3f497f0d4a52b
calm-fork-7535 <komald.4@gmail.com>
2020-01-22 21:57:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,17 +22,17 @@ end
 def directors_totals(nds)
   result = {}
   director_index = 0
-  director_index = 0 
+  director_index = 0
 
    while director_index < nds.count do
-     movie_index = 0 
-     gross_total = 0 
+     movie_index = 0
+     gross_total = 0
      while movie_index < nds[director_index][:movies].length do
        gross_total += nds[director_index][:movies][movie_index][:worldwide_gross]
-       movie_index += 1 
+       movie_index += 1
      end
      result[nds[director_index][:name]] = gross_total
-     director_index += 1 
+     director_index += 1
    end
    result
  end	end
